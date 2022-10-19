38852a23a4e9795eef0ee6565b45dd47b9b52ae2
Abhinav G <56132559+Abh1noob@users.noreply.github.com>
2022-10-19 10:28:52 +0530

    Update Matrix Calculator Abh1noob.py
    Added Subtraction Function and improved the Interface and made it more geometrical and uniform

--- Matrix Calculator Abh1noob.py
+++ Matrix Calculator Abh1noob.py
@@ -1,18 +1,35 @@
-#Matrix Addition wit(int(h t.get()))kr rot
+#Matrix Addition with tkinter
 
+#importing Libraries
 import tkinter as tk
 from tkinter import *
 
-def calculate():
-    tk.Label(root,text=(int(a11.get()))+(int(b11.get()))).place(y='5',x='235')
-    tk.Label(root,text=(int(a12.get()))+(int(b12.get()))).place(y='5',x='275')
-    tk.Label(root,text=(int(a21.get()))+(int(b21.get()))).place(y='30',x='235')
-    tk.Label(root,text=(int(a22.get()))+(int(b22.get()))).place(y='30',x='275')
 
+
+#Functions
+def addition():
+    tk.Label(root,text=(int(a11.get()))+(int(b11.get())), bg="white", width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
+    tk.Label(root,text=(int(a12.get()))+(int(b12.get())), bg="white", width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,text=(int(a21.get()))+(int(b21.get())), bg="white", width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
+    tk.Label(root,text=(int(a22.get()))+(int(b22.get())), bg="white", width = '5',borderwidth=2, relief="groove").place(y='40',x='300')
+    tk.Label(root,text= "+").place(y='24',x='108')   
+
+def subtraction():
+    tk.Label(root,text=(int(a11.get()))-(int(b11.get())), bg = "white", width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
+    tk.Label(root,text=(int(a12.get()))-(int(b12.get())), bg = "white", width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,text=(int(a21.get()))-(int(b21.get())), bg = "white", width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
+    tk.Label(root,text=(int(a22.get()))-(int(b22.get())), bg = "white", width = '5',borderwidth=2, relief="groove").place(y='40',x='300')
+    tk.Label(root,text= "-").place(y='24',x='108')
+
+def multiplication():
+    pass
+
+#Initialising tkinter
 root = tk.Tk()
-root.geometry("400x100")
+root.geometry("350x100")
 root.title("Matrix Calculator")
 
+#Declaring entries to be variable
 a11 = tk.Variable
 a12 = tk.Variable
 a21 = tk.Variable
@@ -23,29 +40,31 @@ b12 = tk.Variable
 b21 = tk.Variable
 b22 = tk.Variable
 
-a11 = tk.Entry(root, text = 'a11',font=('calibre',10,'normal'),width=5)
-a12 = tk.Entry(root, text = 'a12',font=('calibre',10,'normal'),width=5)
-a21 = tk.Entry(root, text = 'a21',font=('calibre',10,'normal'),width=5)
-a22 = tk.Entry(root, text = 'a22',font=('calibre',10,'normal'),width=5)
-b11 = tk.Entry(root, text = 'b11',font=('calibre',10,'normal'),width=5)
-b12 = tk.Entry(root, text = 'b12',font=('calibre',10,'normal'),width=5)
-b21 = tk.Entry(root, text = 'b21',font=('calibre',10,'normal'),width=5)
-b22 = tk.Entry(root, text = 'b22',font=('calibre',10,'normal'),width=5)
+#Taking Entries through tkinter
+a11 = tk.Entry(root, text = 'a11',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+a12 = tk.Entry(root, text = 'a12',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+a21 = tk.Entry(root, text = 'a21',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+a22 = tk.Entry(root, text = 'a22',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+b11 = tk.Entry(root, text = 'b11',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+b12 = tk.Entry(root, text = 'b12',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+b21 = tk.Entry(root, text = 'b21',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
+b22 = tk.Entry(root, text = 'b22',font=('calibre',10,'normal'),width=5,borderwidth=2, relief="groove")
 
-a11.place(x=5, y = 5)
-a12.place(x=50, y = 5)
-a21.place(x=5, y = 30)
-a22.place(x=50, y = 30)
+#Placing of Components in x-y plane
+a11.place(x=10, y = 10)
+a12.place(x=60, y = 10)
+a21.place(x=10, y = 40)
+a22.place(x=60, y = 40)
 
-b11.place(x=120, y = 5)
-b12.place(x=165, y = 5)
-b21.place(x=120, y = 30)
-b22.place(x=165, y = 30)
+b11.place(x=130, y = 10)
+b12.place(x=180, y = 10)
+b21.place(x=130, y = 40)
+b22.place(x=180, y = 40)
 
-tk.Label(root,text="+").place(y='13',x='98')
+tk.Label(root,text="=").place(y='24',x = '230')
 
-tk.Label(root,text ="=").place(y='13',x = '213')
-
-cal=tk.Button(root,text='Calculate',command=calculate).place(y='60',x='150')
+#Operation Buttons
+add=tk.Button(root,text='Addition',command=addition,width = 10).place(y='70',x='10')
+sub=tk.Button(root,text='Subtraction',command=subtraction,width = 10).place(y='70',x='95')
 
 root.mainloop()
