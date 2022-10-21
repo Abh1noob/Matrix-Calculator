85fe2550ebf1e84b4078a3a5658f30b991e296e5
Abhinav G <56132559+Abh1noob@users.noreply.github.com>
2022-10-21 23:52:47 +0530

    Update Matrix Calculator Abh1noob.py
    Added Function for addition and subtraction of 3x3 matrix, declared new matrix indexes as variables and entries for them created. Placement for 3x3 to be done.

--- Matrix Calculator Abh1noob.py
+++ Matrix Calculator Abh1noob.py
@@ -1,6 +1,7 @@
 #Matrix Addition with tkinter
 
 #importing Libraries
+from sunau import AUDIO_FILE_ENCODING_ADPCM_G723_3
 import tkinter as tk
 from tkinter import *
 
@@ -11,21 +12,48 @@ accent_color="3C4048"
 my_font = ('calibre',10,'bold')
 
 #Functions
-def addition():
+def addition_2_2():
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a11.get()))+(int(b11.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a12.get()))+(int(b12.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a21.get()))+(int(b21.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a22.get()))+(int(b22.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='300')
-    tk.Label(root,fg = fg_color, bg = bg_color,text= "+").place(y='24',x='108')   
+    tk.Label(root,fg = fg_color, bg = bg_color,text= "+").place(y='24',x='108')
 
-def subtraction():
+#placement to be done for addition_3_3
+def addition_3_3():
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a11.get()))+(int(b11.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a12.get()))+(int(b12.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a13.get()))+(int(b13.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a21.get()))+(int(b21.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a22.get()))+(int(b22.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a23.get()))+(int(b23.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a31.get()))+(int(b31.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a32.get()))+(int(b32.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a33.get()))+(int(b33.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color, bg = bg_color,text= "+").place(y='24',x='108')
+
+
+def subtraction_2_2():
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a11.get()))-(int(b11.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a12.get()))-(int(b12.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a21.get()))-(int(b21.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
     tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a22.get()))-(int(b22.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='300')
     tk.Label(root,fg = fg_color, bg = bg_color,text= "-").place(y='24',x='108')
 
-def multiplication():
+#placement to be done for subtraction_3_3
+def subtraction_3_3():
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a11.get()))-(int(b11.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a12.get()))-(int(b12.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a13.get()))-(int(a13.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a21.get()))-(int(b21.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a22.get()))-(int(b22.get())),width = '5',borderwidth=2, relief="groove").place(y='40',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a23.get()))-(int(b23.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a31.get()))-(int(b31.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a32.get()))-(int(b32.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color,bg = bg_color,text=(int(a33.get()))-(int(b33.get())),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
+    tk.Label(root,fg = fg_color, bg = bg_color,text= "-").place(y='24',x='108')
+
+def multiplication_2_2():
     tk.Label(root,fg = fg_color,bg = bg_color, text=(((int(a11.get()))*(int(b11.get())))+((int(a12.get()))*(int(b21.get())))),width = '5',borderwidth=2, relief="groove").place(y='10',x='250')
     tk.Label(root,fg = fg_color,bg = bg_color, text=(((int(a11.get()))*(int(b12.get())))+((int(a12.get()))*(int(b22.get())))),width = '5',borderwidth=2, relief="groove").place(y='10',x='300')
     tk.Label(root,fg = fg_color,bg = bg_color, text=(((int(a21.get()))*(int(b11.get())))+((int(a22.get()))*(int(b21.get())))),width = '5',borderwidth=2, relief="groove").place(y='40',x='250')
@@ -41,23 +69,45 @@ root['bg']=bg_color
 #Declaring entries to be variable
 a11 = tk.Variable
 a12 = tk.Variable
+a13 = tk.Variable
 a21 = tk.Variable
 a22 = tk.Variable
+a23 = tk.Variable
+a31 = tk.Variable
+a32 = tk.Variable
+a33 = tk.Variable
 
 b11 = tk.Variable
 b12 = tk.Variable
+b13 = tk.Variable
 b21 = tk.Variable
 b22 = tk.Variable
+b23 = tk.Variable
+b31 = tk.Variable
+b32 = tk.Variable
+b33 = tk.Variable
 
-#Taking Entries through tkinter
+#Taking Entries through tkinter, placement for new stuff to be done
 a11 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a11',font=my_font,width=5,borderwidth=2, relief="groove")
 a12 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a12',font=my_font,width=5,borderwidth=2, relief="groove")
+a13 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a13',font=my_font,width=5,borderwidth=2, relief="groove")
 a21 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a21',font=my_font,width=5,borderwidth=2, relief="groove")
 a22 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a22',font=my_font,width=5,borderwidth=2, relief="groove")
+a23 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a23',font=my_font,width=5,borderwidth=2, relief="groove")
+a31 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a31',font=my_font,width=5,borderwidth=2, relief="groove")
+a32 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a32',font=my_font,width=5,borderwidth=2, relief="groove") 
+a33 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'a33',font=my_font,width=5,borderwidth=2, relief="groove") 
+
 b11 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b11',font=my_font,width=5,borderwidth=2, relief="groove")
 b12 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b12',font=my_font,width=5,borderwidth=2, relief="groove")
+b13 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b13',font=my_font,width=5,borderwidth=2, relief="groove")
 b21 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b21',font=my_font,width=5,borderwidth=2, relief="groove")
 b22 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b22',font=my_font,width=5,borderwidth=2, relief="groove")
+b23 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b23',font=my_font,width=5,borderwidth=2, relief="groove")
+b31 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b31',font=my_font,width=5,borderwidth=2, relief="groove")
+b32 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b32',font=my_font,width=5,borderwidth=2, relief="groove") 
+b33 = tk.Entry(root,justify=CENTER,fg = fg_color, bg = bg_color, text = 'b33',font=my_font,width=5,borderwidth=2, relief="groove") 
+
 
 #Placing of Components in x-y plane
 a11.place(x=10, y = 10)
